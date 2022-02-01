66476c6983d6156d290cffb2e2d9aabe3d4df863
HamidMammadov <hamidvm@code.edu.az>
2022-02-01 17:51:46 +0400

    First Commit

---
+++ Array/script.js
@@ -0,0 +1,50 @@
+// let arr = [1,2,3,5,7,8,15,17];
+// let n = 9999;
+// let check = true;
+
+// for (let i = 0; i < arr.length; i++) {
+
+//     if (arr[i] > n) {
+//         console.log("TRUE");
+//         check = false;
+//         break;
+//     }
+    
+// }
+
+// if (check) {
+//     console.log("FALSE");
+// }
+
+// let arr = [1,2,4];
+
+// console.log(arr[2])
+
+// for (let i = 0; i < arr.length; i++) {
+
+//     if(i == 2){
+//         arr[i]
+//     }
+    
+// }
+
+// let i = 0;
+// for (let i = 0; i < arr.length; i++)
+// {
+//     if(arr[i] % 2 != 0)
+//     {
+//         console.log("FALSE");
+//         break;
+//     }
+// }
+
+
+// while (i < arr.length)
+// {
+//     if (arr[i] % 2 == 0) {
+//         i++;
+//     }else{
+//         console.log("False");
+//     }
+// }
+
